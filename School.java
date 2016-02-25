91da092ecd4515061ad6f9749f732f9383fed03d
jcblomquist <jcblomquist@lin17.cs.csbsju.edu>
2016-02-24 19:55:24 -0600

    Adding half of school instance variables

--- School.java
+++ School.java
@@ -7,5 +7,15 @@
  *
  */
 public class School {
-
+	
+	private int satMath;
+	private int expenses;
+	private int perFinancial;
+	private int numApplicants;
+	private int perAdmitted;
+	private int perEnrolled;
+	private int acadScale;
+	private int socialScale;
+	private int QOLScale;
+	
 }
