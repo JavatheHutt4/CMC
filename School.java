81d3ce3e889bc97e391ae7668aeffcd1b219ba2a
tdrichmond <tdrichmond@lin16.cs.csbsju.edu>
2016-02-24 19:58:18 -0600

    Committing instance variables in School class

--- School.java
+++ School.java
@@ -8,6 +8,13 @@
  */
 public class School {
 	
+	private String name;
+	private String state;
+	private String location;
+	private String control;
+	private int numStudents;
+	private int perFemale;
+	private int satVerbal;
 	private int satMath;
 	private int expenses;
 	private int perFinancial;
