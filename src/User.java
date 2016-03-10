9de82a3275a5a7a5f0e9d5015b58bdd2a8a4dc0b
tdrichmond <tdrichmond@linpe218vm12.csbsju.edu>
2016-03-09 23:25:15 -0600

    User update

--- src/User.java
+++ src/User.java
@@ -8,7 +8,7 @@
  */
 public class User extends Member{
 	public User(String firstName, String lastName, String userName, String password, String type, String status) {
-		super(firstName, lastName, userName, password, type, status);
+		super(firstName, lastName, userName, password, "u", status);
 	}
 
 }
