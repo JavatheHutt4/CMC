f142defb44f5f864bebb01ae63be5f962fb7e1f9
jcblomquist <jcblomquist@linpe218vm18.csbsju.edu>
2016-03-09 23:22:20 -0600

    AdminUI and AdminHome

---
+++ src/AdminHome.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author jcblomquist
+ *
+ */
+public class AdminHome {
+	public Admin admin;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/AdminUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author jcblomquist
+ *
+ */
+public class AdminUI {
+	public AdminHome adminHome;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
