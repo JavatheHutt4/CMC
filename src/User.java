6f25244e8ae827c8b1177573f53860ffafb23759
tdrichmond <tdrichmond@linpe218vm12.csbsju.edu>
2016-03-09 22:58:01 -0600

    Updated variables in Admin and Member

--- src/User.java
+++ src/User.java
@@ -7,20 +7,6 @@
  *
  */
 public class User extends Member{
-	private String firstName;
-	private String lastName;
-	private String userName;
-	private String password;
-	private String type;
-	private String status;
-	/**
-	 * @param firstName
-	 * @param lastName
-	 * @param userName
-	 * @param password
-	 * @param type
-	 * @param status
-	 */
 	public User(String firstName, String lastName, String userName, String password, String type, String status) {
 		super(firstName, lastName, userName, password, type, status);
 	}
