1f823654c50c5fc76430e5b79e69a52668f2fde6
jcblomquist <jcblomquist@linpe218vm18.csbsju.edu>
2016-03-09 23:00:42 -0600

    admin changes

--- src/Admin.java
+++ src/Admin.java
@@ -24,5 +24,16 @@ public class Admin extends Member {
 	public Admin(String firstName, String lastName, String userName, String password, String type, String status) {
 		super(firstName, lastName, userName, password, type, status);
 	}
-
+	
+	/**
+	 * Retrieves password for a given Admin
+	 * @return String password
+	 */
+	public getPassword(){
+	}
+	
+	/**
+	 * 
+	 */
+	
 }
