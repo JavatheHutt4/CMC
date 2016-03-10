8955b81ee761062de743de124649c3dfe9ff51b1
tdrichmond <tdrichmond@linpe218vm12.csbsju.edu>
2016-03-09 22:03:17 -0600

    Updated Member.java

--- src/Member.java
+++ src/Member.java
@@ -7,7 +7,12 @@
  *
  */
 public abstract class Member {
-	private String name;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String type;
+	private String status;
 	
 
 }
