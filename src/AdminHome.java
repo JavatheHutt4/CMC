bfe1e2b30f1ad77f15c6730d3a33f91e18929ddb
Jacob Fritz <jacobffritz@gmail.com>
2016-03-10 20:45:59 -0600

    Added the methods to adminHome

--- src/AdminHome.java
+++ src/AdminHome.java
@@ -8,7 +8,7 @@
  */
 public class AdminHome {
 	public Admin admin;
-
+	
 	/**
 	 * @param args
 	 */
@@ -16,5 +16,32 @@ public class AdminHome {
 		// TODO Auto-generated method stub
 
 	}
-
+	/**
+	 * Deactivates the member by setting its type to "N"
+	 */
+	public void deactivateMember(Member m)
+	{
+		m.deactivateMember();
+	}
+	/*
+	 * adds a new member with all the information provided in the parameter
+	 */
+	public void addNewMember(String u, String f, String l, String p, String t, String s){
+		if(t != "a" && t != "u" && s != "Y" && s != "N"){
+			System.out.print("Invalid new member information");
+		}
+			Member member = new Member(f, l, u, p, t, s);
+	}
+	/*
+	 *  edits the members info in occordance to the information in the parameters
+	 */
+	public void editMemberInformation(String f, String l, String u, String p, String t, String s){
+		this.setFirstName(f);
+		this.setLastName(l);
+		this.setUserName(u);
+		this.setPassword(p);
+		this.setType(t);
+		this.setStatus(s);
+	}
+	
 }
