65031ae4096499179a724f6c7a7e7b7e5ede458b
Jacob Fritz <jacobffritz@gmail.com>
2016-03-13 12:42:57 -0500

    finished adding the empty methods. TODO fill the with code

--- src/UserUI.java
+++ src/UserUI.java
@@ -17,5 +17,59 @@ public class UserUI {
 		// TODO Auto-generated method stub
 
 	}
-
+	/*
+	 * updates the users info through the user opbject
+	 */
+	public void manageMyProfile(String f, String l, String p)
+	{
+		user.updateInformation(f,l,p);
+	}
+	public void search(String sn, String s, String l, String c, String ns, Sting pf, String satv, String satm, String e, String fa, String na, String pa, String pe, String ac, String ss, String qls, String em)
+	{
+		/*
+		 * TODO implement search function
+		 */
+	}
+	public School recommendSchool(School s)
+	{
+		/*
+		 * TODO implement search Function
+		 */
+	}
+	public void manageSavedSchools()
+	{
+		
+	}
+	public void saveSchool(String s)
+	{
+		
+	}
+	public void confirmSave()
+	{
+		
+	}
+	public void viewSpecificSchool(String n)
+	{
+		
+	}
+	public void logoff()
+	{
+		
+	}
+	public void removeSavedSchool(String n)
+	{
+		
+	}
+	public void confirmChanges()
+	{
+		
+	}
+	public void cancelChanges()
+	{
+		
+	}
+	public void requestConfirmation()
+	{
+		
+	}
 }
