cfd7c5ff72087b731cbf97cd0d928879ba5e8834
tdrichmond <tdrichmond@linclemensvm6.csbsju.edu>
2016-03-13 21:22:47 -0500

    Javadocs

--- src/AdminHome.java
+++ src/AdminHome.java
@@ -20,21 +20,21 @@ public class AdminHome {
 	/*
 	 * adds a new member with all the information provided in the parameter
 	 */
-	public void addNewMember(String u, String f, String l, String p, char t,){
+	public void addNewMember(String u, String f, String l, String p, char t)
+	{
 		if(t != 'a' && t != 'u'){
 			System.out.print("Invalid new member information");
 		}
 		databasecontroller.addNewMember(f, l, u, p, t);
 	}
 	/*
-	 *  edits the members info in occordance to the information in the parameters
+	 *  edits the members info in accordance to the information in the parameters
 	 */
-	public void editMemberInformation(String f, String l, String u, String p, char t, String s)
+	public void editMemberInformation(String f, String l, String u, String p, char t, char s)
 	{
-		if(t != 'a' || t != 'u' && s != "N" || s != "A"){
+		if(t != 'a' || t != 'u' && s != 'N' || s != 'A'){
 			System.out.print("Invalid new member information");
 		}
 		databasecontroller.editUser(f, l, u, p, t, s);
-	}
 	}
 }
