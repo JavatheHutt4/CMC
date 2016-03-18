54909e94e5ffc9b0388e2b80d6e0ae319c46640f
jcblomquist <jcblomquist@linclemensvm4.csbsju.edu>
2016-03-18 13:05:11 -0500

    Adding my test classes

---
+++ tests/CMCPackage/DatabaseControllerTest.java
@@ -0,0 +1,83 @@
+package CMCPackage;
+
+/**
+ * @author jcblomquist
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabaseControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDatabaseController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditSchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSchools() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSchoolsWithEmphases() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveSchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUniversityEmphasis() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsernamesWithSavedSchools() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/CMCPackage/UserTest.java
@@ -0,0 +1,38 @@
+package CMCPackage;
+
+/**
+ * @author jcblomquist
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateInformation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSavedSchools() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewSavedSchool() {
+		fail("Not yet implemented");
+	}
+
+}
