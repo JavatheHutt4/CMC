a7b844ad8fa571820dc6e68c3a1eb29d300ff97f
jrfolkerds <jrfolkerds@lin5.cs.csbsju.edu>
2016-04-03 21:03:34 -0500

    Created Test Suite

---
+++ tests/CMCPackage/TestSuite.java
@@ -0,0 +1,26 @@
+
+package CMCPackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	AdminHomeTest.class,
+	AdminTest.class,
+	DatabaseControllerTest.class,
+	LogonControllerTest.class,
+	SchoolHomeTest.class,
+	SchoolTest.class,
+	SearchControllerTest.class,
+	UserHomeTest.class,
+	UserTest.class,
+	UserUITest.class
+})
+
+/**
+ * @author jrfolkerds
+ * @version 4/3/2016
+ */
+public class TestSuite {
+
+}
